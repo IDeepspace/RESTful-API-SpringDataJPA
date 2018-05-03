4543731746286efb618c78a773c752a6b238d669
IDeepspace <cxin1427@qq.com>
2018-05-03 22:12:39 +0800

    API-获取employee列表

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,25 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import java.util.*;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
-    //在此处完成Company API
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @GetMapping(value = "")
+  public ResponseEntity getCompanies() throws Exception {
+
+    List<Company> companyList = companyRepository.findAll();
+
+    return new ResponseEntity<>(companyList, HttpStatus.OK);
+  }
 }
