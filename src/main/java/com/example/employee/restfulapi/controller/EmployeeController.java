1e03d398f00694913e18c8a834e84686cd90d831
IDeepspace <cxin1427@qq.com>
2018-05-04 11:33:45 +0800

    API-获取employee列表

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,30 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import java.util.*;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
-    //在此处完成Employee API
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @GetMapping(value = "")
+  public ResponseEntity getAllEmployees() throws Exception {
+    List<Employee> employeeList = employeeRepository.findAll();
+
+    return new ResponseEntity<>(employeeList, HttpStatus.OK);
+  }
 }
