f4115c9efc061da6592581c89338b7e892f790ec
IDeepspace <cxin1427@qq.com>
2018-05-04 12:15:18 +0800

    更新某个employee

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -55,5 +55,19 @@ public class EmployeeController {
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
+  @PutMapping(value = "/{id}")
+  public ResponseEntity updateEmployee(@PathVariable Long id, @RequestBody Employee newEmployee) throws Exception {
+    Employee employee = employeeRepository.findOne(id);
+    
+    employee.setName(newEmployee.getName());
+    employee.setAge(newEmployee.getAge());
+    employee.setGender(newEmployee.getGender());
+    employee.setSalary(newEmployee.getSalary());
+    employee.setCompanyId(newEmployee.getCompanyId());
+
+    employeeRepository.save(employee);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
 
 }
